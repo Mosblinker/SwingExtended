2ed328dfdb0eeb0d332e78f0d0da05f57c87039e
WALLE1Doctor1Who <msteier2.0@gmail.com>
2024-10-11 15:06:29 -0500

    ArrayComboBoxModel documentation update
    I've updated the comments in ArrayComboBoxModel.

--- src/components/ArrayComboBoxModel.java
+++ src/components/ArrayComboBoxModel.java
@@ -11,11 +11,13 @@ import javax.swing.*;
 /**
  *
  * @author Milo Steier
- * @param <E>
+ * @param <E> The type of elements to be stored in this list.
  */
 public class ArrayComboBoxModel<E> extends ArrayListModel<E> implements 
         MutableComboBoxModel<E>{
-    
+    /**
+     * This is the currently selected object for this model.
+     */
     private Object selected = null;
     
     public ArrayComboBoxModel(){
@@ -35,9 +37,9 @@ public class ArrayComboBoxModel<E> extends ArrayListModel<E> implements
     public ArrayComboBoxModel(E[] arr){
         this(Arrays.asList(arr));
     }
-    
     @Override
     public void add(int index, E element){
+            // Add the element to the list
         super.add(index, element);
             // If the list was previously empty, the selected item is not set, 
             // and the element that was added was not null
@@ -65,23 +67,36 @@ public class ArrayComboBoxModel<E> extends ArrayListModel<E> implements
     }
     @Override
     protected void removeRange(int fromIndex, int toIndex){
+            // Get the index of the selected item if a non-null item is selected
         int selIndex = (selected != null) ? indexOf(selected) : -1;
+            // Remove the values in the range
         super.removeRange(fromIndex, toIndex);
+            // If the selected item was one of the items that were removed
         if (selIndex >= fromIndex && selIndex <= toIndex)
+                // Clear the selected item
             setSelectedItem(null);
     }
     @Override
     protected boolean removeIf(Predicate<? super E> filter, int fromIndex, int toIndex){
+            // Remove the elements in the given range that match the given 
+            // filter
         boolean modified = super.removeIf(filter, fromIndex, toIndex);
+            // If the list was modified, the selected item is not null, and the 
+            // selected item is no longer in this list
         if (modified && selected != null && !contains(selected))
+                // Clear the selected item
             setSelectedItem(null);
         return modified;
     }
     @Override
     protected boolean batchRemove(Collection<?> c, boolean retain, 
             int fromIndex, int toIndex){
+            // Remove the elements in the given range that should be removed 
         boolean modified = super.batchRemove(c, retain, fromIndex, toIndex);
+            // If the list was modified, the selected item is not null, and the 
+            // selected item would not have been retained
         if (modified && selected != null && c.contains(selected) != retain)
+                // Clear the selected item
             setSelectedItem(null);
         return modified;
     }
@@ -95,15 +110,22 @@ public class ArrayComboBoxModel<E> extends ArrayListModel<E> implements
     }
     @Override
     public E set(int index, E element){
+            // Set the item in the list at the given index
         E old = super.set(index, element);
+            // If the item previously at that index was selected
         if (old == selected)
+                // Set the selected item to the new element
             setSelectedItem(element);
         return old;
     }
     @Override
     public void setSelectedItem(Object anItem) {
+            // If the currently selected item does not match the given object
         if (!Objects.equals(selected, anItem)){
+                // Set the selected item
             selected = anItem;
+                // Fire a contents changed event indicating that the selected
+                // item has changed
             fireContentsChanged(-1,-1);
         }
     }
@@ -111,5 +133,4 @@ public class ArrayComboBoxModel<E> extends ArrayListModel<E> implements
     public Object getSelectedItem() {
         return selected;
     }
-    
 }
