62e55ea5339ec0b868e90fa0935aa0e1350c14de
WALLE1Doctor1Who <msteier2.0@gmail.com>
2024-10-11 14:42:47 -0500

    ListModelList now extends AbstractListModelList
    I've made it so that ListModelList now extends AbstractListModelList.

--- src/components/ListModelList.java
+++ src/components/ListModelList.java
@@ -41,16 +41,12 @@ import javax.swing.event.*;
  * @param <E> The data type of the elements for the list.
  * @see components.ComboBoxModelList
  */
-public class ListModelList <E> extends AbstractList<E> implements List<E>, ListModel<E>{
+public class ListModelList <E> extends AbstractListModelList<E>{
     /**
      * The underlying ListModel used to store the contents of this list, and is 
      * what this list delegates its methods to.
      */
     private final ListModel<? extends E> model;
-    /**
-     * The list of EventListeners registered to this model.
-     */
-    protected EventListenerList listenerList;
     /**
      * This constructs a ListModelList with the given ListModel. 
      * @param model The ListModel that this maps the List methods to (cannot be 
@@ -59,7 +55,6 @@ public class ListModelList <E> extends AbstractList<E> implements List<E>, ListM
      */
     public ListModelList(ListModel<? extends E> model){
         this.model = Objects.requireNonNull(model, "ListModel cannot be null");
-        listenerList = new EventListenerList();
         model.addListDataListener(new Handler());
     }
     /**
@@ -125,178 +120,6 @@ public class ListModelList <E> extends AbstractList<E> implements List<E>, ListM
         return temp;
     }
     /**
-     * This returns the number of elements in this list. This method is 
-     * identical to {@link #size() size}.
-     * @return The number of elements in this list.
-     * @see #size() 
-     */
-    @Override
-    public int getSize() {
-        return size();
-    }
-    /**
-     * This returns the element at the specified index in this list. This method 
-     * is identical to {@link #get(int) get}, which is the preferred method to 
-     * use.
-     * @param index The index of the element to return.
-     * @return The element at the specified index in this list.
-     * @throws IndexOutOfBoundsException If the index is out of bounds. {@code 
-     * (index < 0 || index >= size())}
-     * @see #get(int) 
-     */
-    @Override
-    public E getElementAt(int index) {
-        return get(index);
-    }
-    /**
-     * This adds a {@code ListDataListener} to this ListModelList that will be 
-     * notified when the structure of this list is modified.
-     * @param l {@inheritDoc }
-     * @see #removeListDataListener(ListDataListener) 
-     * @see #getListDataListeners() 
-     */
-    @Override
-    public void addListDataListener(ListDataListener l) {
-        if (l != null)  // If the listener is not null
-            listenerList.add(ListDataListener.class, l);
-    }
-    /**
-     * This removes a {@code ListDataListener} from this ListModelList.
-     * @param l {@inheritDoc }
-     * @see #addListDataListener(ListDataListener) 
-     * @see #getListDataListeners() 
-     */
-    @Override
-    public void removeListDataListener(ListDataListener l) {
-        listenerList.remove(ListDataListener.class, l);
-    }
-    /**
-     * This returns an array of all the {@code ListDataListener}s that have been 
-     * registered to this ListModelList.
-     * @return An array containing the {@code ListDataListener}s that have been 
-     * added, or an empty array if no listeners have been added.
-     * @see #addListDataListener(ListDataListener) 
-     * @see #removeListDataListener(ListDataListener) 
-     */
-    public ListDataListener[] getListDataListeners(){
-        return listenerList.getListeners(ListDataListener.class);
-    }
-    /**
-     * This returns an array of all the objects currently registered as 
-     * <code><em>Foo</em>Listener</code>s on this model. 
-     * <code><em>Foo</em>Listener</code>s are registered via the 
-     * <code>add<em>Foo</em>Listener</code> method. <p>
-     * 
-     * The listener type can be specified using a class literal, such as 
-     * <code><em>Foo</em>Listener.class</code>. If no such listeners exist, 
-     * then an empty array will be returned.
-     * @param <T> The type of {@code EventListener} being requested.
-     * @param listenerType The type of listeners being requested. This should 
-     * be an interface that descends from {@code EventListener}.
-     * @return An array of the objects registered as the given listener type on 
-     * this model, or an empty array if no such listeners have been added.
-     * @see #getListDataListeners() 
-     */
-    public <T extends EventListener> T[] getListeners(Class<T> listenerType){
-        return listenerList.getListeners(listenerType);
-    }
-    /**
-     * This fires a ListDataEvent of the given type over the interval between 
-     * {@code index0} and {@code index1}, inclusive. Note that {@code index0} 
-     * does not need to be less than or equal to {@code index1}.
-     * @param type The type of event to be fired. 
-     * @param index0 One end of the interval.
-     * @param index1 The other end of the interval.
-     * @see #getListDataListeners() 
-     * @see ListDataEvent
-     * @see ListDataListener
-     * @see #fireContentsChanged(int, int) 
-     * @see #fireIntervalAdded(int, int) 
-     * @see #fireIntervalRemoved(int, int) 
-     */
-    protected void fireListDataEvent(int type, int index0, int index1){
-            // This creates the event to be fired.
-        ListDataEvent evt = new ListDataEvent(this,type,index0,index1);
-            // Go through the ListDataListeners
-        for (ListDataListener l : listenerList.getListeners(ListDataListener.class)){
-            if (l != null){     // If the current listener is not null
-                switch(type){
-                    case(ListDataEvent.INTERVAL_ADDED):     // If an interval was added
-                        l.intervalAdded(evt);
-                        break;
-                    case(ListDataEvent.INTERVAL_REMOVED):   // If an interval was removed
-                        l.intervalRemoved(evt);
-                        break;
-                    case(ListDataEvent.CONTENTS_CHANGED):   // If the contents changed
-                        l.contentsChanged(evt);
-                }
-            }
-        }
-    }
-    /**
-     * This fires a ListDataEvent informing the listeners that the contents of 
-     * the model have changed within the interval between {@code index0} and 
-     * {@code index1}, inclusive. Note that {@code index0} does not need to be 
-     * less than or equal to {@code index1}. This effectively calls {@link 
-     * #fireListDataEvent fireListDataEvent} with the given interval and with 
-     * {@link ListDataEvent#CONTENTS_CHANGED CONTENTS_CHANGED} as the {@code 
-     * type} of event.
-     * @param index0 One end of the interval.
-     * @param index1 The other end of the interval.
-     * @see #getListDataListeners() 
-     * @see ListDataEvent
-     * @see ListDataListener
-     * @see #fireListDataEvent(int, int, int) 
-     * @see #fireIntervalAdded(int, int) 
-     * @see #fireIntervalRemoved(int, int) 
-     * @see ListDataEvent#CONTENTS_CHANGED
-     */
-    protected void fireContentsChanged(int index0, int index1){
-        fireListDataEvent(ListDataEvent.CONTENTS_CHANGED,index0,index1);
-    }
-    /**
-     * This fires a ListDataEvent informing the listeners that elements have 
-     * been added to the model within the interval between {@code index0} and 
-     * {@code index1}, inclusive. Note that {@code index0} does not need to be 
-     * less than or equal to {@code index1}. This effectively calls {@link 
-     * #fireListDataEvent fireListDataEvent} with the given interval and with 
-     * {@link ListDataEvent#INTERVAL_ADDED INTERVAL_ADDED} as the {@code type} 
-     * of event.
-     * @param index0 One end of the interval.
-     * @param index1 The other end of the interval.
-     * @see #getListDataListeners() 
-     * @see ListDataEvent
-     * @see ListDataListener
-     * @see #fireListDataEvent(int, int, int) 
-     * @see #fireContentsChanged(int, int) 
-     * @see #fireIntervalRemoved(int, int) 
-     * @see ListDataEvent#INTERVAL_ADDED
-     */
-    protected void fireIntervalAdded(int index0, int index1){
-        fireListDataEvent(ListDataEvent.INTERVAL_ADDED,index0,index1);
-    }
-    /**
-     * This fires a ListDataEvent informing the listeners that elements have 
-     * been removed from the model that use to be within the interval between 
-     * {@code index0} and {@code index1}, inclusive. Note that {@code index0} 
-     * does not need to be less than or equal to {@code index1}. This 
-     * effectively calls {@link #fireListDataEvent fireListDataEvent} with the 
-     * given interval and with {@link ListDataEvent#INTERVAL_REMOVED 
-     * INTERVAL_REMOVED} as the {@code type} of event.
-     * @param index0 One end of the interval.
-     * @param index1 The other end of the interval.
-     * @see #getListDataListeners() 
-     * @see ListDataEvent
-     * @see ListDataListener
-     * @see #fireListDataEvent(int, int, int) 
-     * @see #fireContentsChanged(int, int) 
-     * @see #fireIntervalAdded(int, int) 
-     * @see ListDataEvent#INTERVAL_REMOVED
-     */
-    protected void fireIntervalRemoved(int index0, int index1){
-        fireListDataEvent(ListDataEvent.INTERVAL_REMOVED,index0,index1);
-    }
-    /**
      * This checks to see if the given index is valid, and if not, this will 
      * throw an IndexOutOfBoundsException. The index is valid if the index is 
      * greater than or equal to zero, and the index is less than the size of 
@@ -325,28 +148,6 @@ public class ListModelList <E> extends AbstractList<E> implements List<E>, ListM
      */
     private void checkIndex(int index){
         checkIndex(index,false);
-    }
-    /**
-     * This checks to see if the {@code fromIndex} and {@code toIndex} are 
-     * within range. The indexes are in range if they are within bounds and the 
-     * {@code fromIndex} is less than or equal to the {@code toIndex}. If the 
-     * range is out of bounds, then this will throw an 
-     * IndexOutOfBoundsException.
-     * @param fromIndex The index to start at.
-     * @param toIndex The index to stop at, exclusive.
-     * @throws IndexOutOfBoundsException If the range is out of bounds.
-     */
-    private void checkRange(int fromIndex, int toIndex){
-        if (fromIndex < 0)          // If the start index is negative
-            throw new IndexOutOfBoundsException("From Index out of bounds: " + 
-                    fromIndex + " < 0");
-        else if (toIndex > size())  // If the end index is greater than the size
-            throw new IndexOutOfBoundsException("To Index out of bounds: " + 
-                    toIndex + " > " + size());
-            // If the start index is greater than the stop index
-        else if (fromIndex > toIndex)   
-            throw new IndexOutOfBoundsException("From Index > To Index: "+
-                    fromIndex+" > "+toIndex);
     }
     /**
      * This returns the element at the specified index in this list. This method 
@@ -664,7 +465,7 @@ public class ListModelList <E> extends AbstractList<E> implements List<E>, ListM
      */
     @Override
     public boolean addAll(Collection<? extends E> c){
-        return addAll(size(),c);
+        return super.addAll(c);
     }
     /**
      * This method attempts to remove the element at the given index from the 
@@ -966,7 +767,7 @@ public class ListModelList <E> extends AbstractList<E> implements List<E>, ListM
      */
     @Override
     public void removeRange(int fromIndex, int toIndex){
-        checkRange(fromIndex, toIndex);
+        checkRange(fromIndex, toIndex,size());
         modCount++;     // Increment the modification count
         removeRange(extractListModel(),fromIndex,toIndex);
     }
