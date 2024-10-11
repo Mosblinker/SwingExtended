9fdd46e3cf0c4af85f49f8c467f5331aa17e1809
WALLE1Doctor1Who <msteier2.0@gmail.com>
2024-10-11 14:37:30 -0500

    ArrayListModel now uses AbstractListModelList
    ArrayListModel now implements the AbstractListModelList abstract class instead of implementing AbstractList and ListModel separately.

--- src/components/ArrayListModel.java
+++ src/components/ArrayListModel.java
@@ -6,23 +6,17 @@ package components;
 
 import java.util.*;
 import java.util.function.*;
-import javax.swing.*;
-import javax.swing.event.*;
 
 /**
  * 
  * @author Milo Steier
  * @param <E> The type of elements to be stored in this list.
  */
-public class ArrayListModel<E> extends AbstractList<E> implements ListModel<E>{
+public class ArrayListModel<E> extends AbstractListModelList<E>{
     /**
      * The ArrayList used to store the elements for this list.
      */
     protected final ArrayList<E> list;
-    /**
-     * The list of EventListeners registered to this model.
-     */
-    protected EventListenerList listenerList;
     /**
      * This constructs an ArrayListModel that uses the given {@code ArrayList} 
      * as its internal list.
@@ -30,7 +24,6 @@ public class ArrayListModel<E> extends AbstractList<E> implements ListModel<E>{
      */
     private ArrayListModel(ArrayList<E> list){
         this.list = list;
-        listenerList = new EventListenerList();
     }
     /**
      * This constructs an empty ArrayListModel.
@@ -55,16 +48,8 @@ public class ArrayListModel<E> extends AbstractList<E> implements ListModel<E>{
         return list.get(index);
     }
     @Override
-    public E getElementAt(int index) {
-        return get(index);
-    }
-    @Override
     public int size() {
         return list.size();
-    }
-    @Override
-    public int getSize() {
-        return size();
     }
     @Override
     public void add(int index, E element){
@@ -86,10 +71,6 @@ public class ArrayListModel<E> extends AbstractList<E> implements ListModel<E>{
             fireIntervalAdded(index,index+c.size()-1);
         }
         return modified;
-    }
-    @Override
-    public boolean addAll(Collection<? extends E> c){
-        return addAll(size(),c);
     }
     @Override
     public E set(int index, E element){
@@ -192,17 +173,6 @@ public class ArrayListModel<E> extends AbstractList<E> implements ListModel<E>{
             // Indicate that the element at the given index has been removed
         fireIntervalRemoved(index,index);
         return value;
-    }
-    @Override
-    public boolean remove(Object o){
-            // Get the first index of the element to be removed
-        int index = indexOf(o);
-            // If the element was not found in this list (the index is negative)
-        if (index < 0)
-            return false;
-            // Remove the first instance of the given element
-        remove(index);
-        return true;
     }
     /**
      * This method handles the removal of batches of elements from the internal 
@@ -515,157 +485,6 @@ public class ArrayListModel<E> extends AbstractList<E> implements ListModel<E>{
         checkRange(fromIndex,toIndex,list.size());
             // Create and return a sublist of this list over the given range
         return new SubList<>(this,fromIndex,toIndex);
-    }
-    /**
-     * This returns an array of all the objects currently registered as 
-     * <code><em>Foo</em>Listener</code>s on this model. 
-     * <code><em>Foo</em>Listener</code>s are registered via the 
-     * <code>add<em>Foo</em>Listener</code> method. <p>
-     * 
-     * The listener type can be specified using a class literal, such as 
-     * <code><em>Foo</em>Listener.class</code>. If no such listeners exist, 
-     * then an empty array will be returned.
-     * @param <T> The type of {@code EventListener} being requested.
-     * @param listenerType The type of listeners being requested. This should 
-     * be an interface that descends from {@code EventListener}.
-     * @return An array of the objects registered as the given listener type on 
-     * this model, or an empty array if no such listeners have been added.
-     * @see #getListDataListeners() 
-     */
-    public <T extends EventListener> T[] getListeners(Class<T> listenerType){
-        return listenerList.getListeners(listenerType);
-    }
-    /**
-     * This adds a {@code ListDataListener} to this model that will be notified 
-     * when the structure of this list is modified.
-     * @param l {@inheritDoc }
-     * @see #removeListDataListener(ListDataListener) 
-     * @see #getListDataListeners() 
-     */
-    @Override
-    public void addListDataListener(ListDataListener l) {
-        if (l != null)  // If the listener is not null
-            listenerList.add(ListDataListener.class, l);
-    }
-    /**
-     * This removes a {@code ListDataListener} from this model.
-     * @param l {@inheritDoc }
-     * @see #addListDataListener(ListDataListener) 
-     * @see #getListDataListeners() 
-     */
-    @Override
-    public void removeListDataListener(ListDataListener l) {
-        listenerList.remove(ListDataListener.class, l);
-    }
-    /**
-     * This returns an array of all the {@code ListDataListener}s that have been 
-     * registered to this model.
-     * @return An array containing the {@code ListDataListener}s that have been 
-     * added, or an empty array if no listeners have been added.
-     * @see #addListDataListener(ListDataListener) 
-     * @see #removeListDataListener(ListDataListener) 
-     */
-    public ListDataListener[] getListDataListeners(){
-        return listenerList.getListeners(ListDataListener.class);
-    }
-    /**
-     * This fires a {@code ListDataEvent} of the given type over the interval 
-     * between {@code index0} and {@code index1}, inclusive. Note that {@code 
-     * index0} does not need to be less than or equal to {@code index1}.
-     * @param type The type of event to be fired. 
-     * @param index0 One end of the interval.
-     * @param index1 The other end of the interval.
-     * @see #getListDataListeners() 
-     * @see ListDataEvent
-     * @see ListDataListener
-     * @see #fireIntervalAdded(int, int) 
-     * @see #fireIntervalRemoved(int, int) 
-     * @see #fireContentsChanged(int, int) 
-     */
-    protected void fireListDataEvent(int type, int index0, int index1){
-            // This creates the event to be fired.
-        ListDataEvent evt = new ListDataEvent(this,type,index0,index1);
-            // Go through the ListDataListeners
-        for (ListDataListener l : getListDataListeners()){
-            if (l != null){     // If the current listener is not null
-                switch(type){
-                        // If an interval was added
-                    case(ListDataEvent.INTERVAL_ADDED):
-                        l.intervalAdded(evt);
-                        break;
-                        // If an interval was removed
-                    case(ListDataEvent.INTERVAL_REMOVED):
-                        l.intervalRemoved(evt);
-                        break;
-                        // If the contents changed
-                    case(ListDataEvent.CONTENTS_CHANGED):
-                        l.contentsChanged(evt);
-                }
-            }
-        }
-    }
-    /**
-     * This fires a {@code ListDataEvent} informing the listeners that elements 
-     * have been added to the model within the interval between {@code index0} 
-     * and {@code index1}, inclusive. Note that {@code index0} does not need to 
-     * be less than or equal to {@code index1}. This effectively calls {@link 
-     * #fireListDataEvent fireListDataEvent} with the given interval and with 
-     * {@link ListDataEvent#INTERVAL_ADDED INTERVAL_ADDED} as the {@code type} 
-     * of event.
-     * @param index0 One end of the interval.
-     * @param index1 The other end of the interval.
-     * @see #getListDataListeners() 
-     * @see ListDataEvent
-     * @see ListDataListener
-     * @see #fireListDataEvent(int, int, int) 
-     * @see #fireIntervalRemoved(int, int) 
-     * @see #fireContentsChanged(int, int) 
-     * @see ListDataEvent#INTERVAL_ADDED
-     */
-    protected void fireIntervalAdded(int index0, int index1){
-        fireListDataEvent(ListDataEvent.INTERVAL_ADDED,index0,index1);
-    }
-    /**
-     * This fires a {@code ListDataEvent} informing the listeners that elements 
-     * have been removed from the model that use to be within the interval 
-     * between {@code index0} and {@code index1}, inclusive. Note that {@code 
-     * index0} does not need to be less than or equal to {@code index1}. This 
-     * effectively calls {@link #fireListDataEvent fireListDataEvent} with the 
-     * given interval and with {@link ListDataEvent#INTERVAL_REMOVED 
-     * INTERVAL_REMOVED} as the {@code type} of event.
-     * @param index0 One end of the interval.
-     * @param index1 The other end of the interval.
-     * @see #getListDataListeners() 
-     * @see ListDataEvent
-     * @see ListDataListener
-     * @see #fireListDataEvent(int, int, int) 
-     * @see #fireIntervalAdded(int, int) 
-     * @see #fireContentsChanged(int, int) 
-     * @see ListDataEvent#INTERVAL_REMOVED
-     */
-    protected void fireIntervalRemoved(int index0, int index1){
-        fireListDataEvent(ListDataEvent.INTERVAL_REMOVED,index0,index1);
-    }
-    /**
-     * This fires a {@code ListDataEvent} informing the listeners that the 
-     * contents of the model have changed within the interval between {@code 
-     * index0} and {@code index1}, inclusive. Note that {@code index0} does not 
-     * need to be less than or equal to {@code index1}. This effectively calls 
-     * {@link #fireListDataEvent fireListDataEvent} with the given interval and 
-     * with {@link ListDataEvent#CONTENTS_CHANGED CONTENTS_CHANGED} as the 
-     * {@code type} of event.
-     * @param index0 One end of the interval.
-     * @param index1 The other end of the interval.
-     * @see #getListDataListeners() 
-     * @see ListDataEvent
-     * @see ListDataListener
-     * @see #fireListDataEvent(int, int, int) 
-     * @see #fireIntervalAdded(int, int) 
-     * @see #fireIntervalRemoved(int, int) 
-     * @see ListDataEvent#CONTENTS_CHANGED
-     */
-    protected void fireContentsChanged(int index0, int index1){
-        fireListDataEvent(ListDataEvent.CONTENTS_CHANGED,index0,index1);
     }
     /**
      * This checks for any elements that have changed in the given sublist of 
