185505fb1dd3e9e340c7ba9eb50d4fce260279d8
Mosblinker <msteier2.0@gmail.com>
2025-05-16 22:13:26 -0500

    Added the library class
    I've added the SwingExtendedUtilities library class. It doesn't contain anything yet, but it will soon enough.

---
+++ src/utils/SwingExtendedUtilities.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ * This is a library class with some additional utilities methods not found in 
+ * {@link javax.swing.SwingUtilities SwingUtilities}
+ * @author Mosblinker
+ * @see javax.swing.SwingUtilities
+ */
+public final class SwingExtendedUtilities {
+    /**
+     * This class cannot be constructed.
+     */
+    private SwingExtendedUtilities() {}
+}
